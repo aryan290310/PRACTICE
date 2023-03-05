7c0da79ad7ccd180d5f04f345932a9afb0d4cc1f
AryanPothapregada <100070571+aryan290310@users.noreply.github.com>
2023-03-05 17:55:05 +0000

    Add files via upload

---
+++ age.js
@@ -0,0 +1,6 @@
+var a = 78;
+if (a >= 18 ) {
+  console.log("you are an adult!");
+} else {
+  console.log("you are a kid!");
+}
---
+++ television.js
@@ -0,0 +1,5 @@
+var price = 400;
+if (price > 200) {
+  price = price * 0.9;
+}
+console.log(price);
